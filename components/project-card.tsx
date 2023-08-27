262fd3d8b3248d25923560c92b4263e077745505
Romeo Noveanre <noveanrer@gmail.com>
2023-08-27 12:07:18 +0700

    feat: Github stats on project card

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,3 +1,4 @@
+import { title } from "process";
 import styles from "~/styles/project-card.module.css";
 import { anchorValidate, domainExtractor } from "~/utils";
 
@@ -28,6 +29,11 @@ export default function ProjectCard(props: ProjectCardProps) {
       { props.url !== "" ? <a className="inline-block truncate" target="_blank" href={anchorValidate(props.url)} onClick={(event) => event.stopPropagation()}>
         {domainExtractor(props.url)}
       </a> : <div className="mt-3"></div> }
+      <div className="flex gap-2">
+        <img src={`https://img.shields.io/github/stars/kraken-afk/${props.title}.svg`} alt="total stars" />
+        <img src={`https://img.shields.io/github/forks/kraken-afk/${props.title}.svg`} alt="total forks" />
+        <img src={`https://img.shields.io/github/watchers/kraken-afk/${props.title}.svg`} alt="total watchers" />
+      </div>
       <div
         style={{ backgroundColor: props.color }}
         className={`lang w-[3rem] h-[1.2rem] my-3 absolute top-[6px] left-[.8rem] z-0`}
